27c9a1b0b0d076c819e662b4b359440bb13b06b5
robertsteele7 <atpcycle7@gmail.com>
2022-11-10 04:39:17 -0600

    exer12

---
+++ ORM-Dapper/DapperDepartmentRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Dapper;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public class DapperDepartmentRepository : IDepartmentRepository
+    {   
+        // field or local variable for making queries to the DB
+        private readonly IDbConnection _connection;
+        
+        public DapperDepartmentRepository(IDbConnection connection)
+        {
+            // constructor
+            _connection = connection;
+        }
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            var depos = _connection.Query<Department>("SELECT * FROM departments");
+
+            return depos;
+        }
+
+        public void InsertDepartment(string newDepartmentName)
+        {
+            _connection.Execute("INSERT INTO DEPARTMENTS (Name) VALUES (@departmentName);",
+            new { departmentName = newDepartmentName });
+        }
+    }
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -3,6 +3,9 @@ using System.Data;
 using System.IO;
 using MySql.Data.MySqlClient;
 using Microsoft.Extensions.Configuration;
+using Org.BouncyCastle.Security;
+using System.Collections.Generic;
+using System.Drawing.Printing;
 
 namespace ORM_Dapper
 {
@@ -10,13 +13,44 @@ namespace ORM_Dapper
     {
         static void Main(string[] args)
         {
+            #region Configuration
             var config = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json")
                 .Build();
 
             string connString = config.GetConnectionString("DefaultConnection");
+            #endregion
+
+            IDbConnection conn = new MySqlConnection(connString);
+            DapperDepartmentRepository repo = new DapperDepartmentRepository(conn);
+
+            Console.WriteLine("Hello user, here are the current departments;");
+            Console.WriteLine("Please press enter ....");
+            Console.ReadLine();
+            var depos = repo.GetAllDepartments();
             
+            
+            Console.WriteLine("Do you want to add a department??????");
+            string userResponse = Console.ReadLine();
+            if (userResponse.ToLower() == "yes")
+            { 
+                Console.WriteLine("What is the name of your new Department");
+                userResponse = Console.ReadLine();
+
+                repo.InsertDepartment(userResponse);
+                Print(repo.GetAllDepartments());
+            }
+
+           Console.WriteLine("Have a great day!!");
+        }
+        private static void Print(IEnumerable<Department> depos)
+        {
+            foreach (var depo in depos)
+            {
+                Console.WriteLine($"Id: {depo.DepartmentId} Name: {depo.Name}");
+
+            }
         }
     }
 }
